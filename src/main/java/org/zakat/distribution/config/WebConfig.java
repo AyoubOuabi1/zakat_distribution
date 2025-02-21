e739d10c46264861629b02332ad4a4e5fff31e33
aouabi <ayoub.ouabi@smart-rx.com>
2025-02-21 21:52:32 +0100

    working on receiver bank

--- src/main/java/org/zakat/distribution/config/WebConfig.java
+++ src/main/java/org/zakat/distribution/config/WebConfig.java
@@ -1,17 +1,19 @@
 package org.zakat.distribution.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.IOException;
 
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String externalDir = "file:/var/www/html/aktion/";
         registry.addResourceHandler("/api/uploads/bank-details/**")
-                .addResourceLocations(externalDir);
+                .addResourceLocations("var/www/html/aktion/");
     }
 
 }
