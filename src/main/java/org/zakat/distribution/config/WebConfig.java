a19244a808450dc2a9e07978e1f885d48a30a40e
aouabi <ayoub.ouabi@smart-rx.com>
2025-02-21 22:38:16 +0100

    working on receiver bank details

--- src/main/java/org/zakat/distribution/config/WebConfig.java
+++ src/main/java/org/zakat/distribution/config/WebConfig.java
@@ -10,5 +10,16 @@ import java.io.IOException;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    /*@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String resourcesDir = null;
+        try {
+            resourcesDir = new ClassPathResource("uploads/bank-details/").getURI().toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        registry.addResourceHandler("/api/uploads/bank-details/**")
+                .addResourceLocations(resourcesDir);
+    }*/
 
 }
